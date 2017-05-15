367bab1473e52d0672cf9086dc4df1f24fb8fe4e
trustinruss <russkranz@yahoo.com>
2017-05-15 15:12:12 -0700

    set up search bar and added first middleware redux-promise

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = '2052a111da47bf0a4f9b419abe271a21';
---
+++ src/containers/search_bar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          //if youre passing a callback around & that call back has a ref to "this",
+          //you need to bind the context. Can also use fat arrow function like before.
+          onChange={this.onInputChange} />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
